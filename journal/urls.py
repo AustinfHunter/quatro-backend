2de8fcfba91e008394b4f873b5702dedfe6d188b
austinfhunter <austinfhunter@gmail.com>
2024-10-28 13:31:45 -0400

    change journal entry url

--- journal/urls.py
+++ journal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("dashboard/", views.UserDashboardView.as_view()),
-    path("<int:id>", views.UserFoodJournalEntryView.as_view()),
+    path("entry/<int:id>", views.UserFoodJournalEntryView.as_view()),
     path("entries/", views.UserFoodJournalEntriesView.as_view()),
     path("entries/create/", views.CreateUserFoodJournalEntryView.as_view()),
 ]
